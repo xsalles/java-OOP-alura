96bdd3f267c31c83419c01bcb29027e188305810
xsalles <xs.salles@gmail.com>
2024-09-17 20:14:08 -0300

    feat: created a method that can show the informations of some student.

--- desafios/src/student/Main.java
+++ desafios/src/student/Main.java
@@ -1,5 +1,21 @@
 package student;
 
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Student myStudent = new Student();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nQual é o nome do estudante?\n");
+        myStudent.nameOfTheStudent = sc.nextLine();
+
+        System.out.println("\nQual a idade desse estudante?\n");
+        myStudent.age = sc.nextInt();
+
+        myStudent.showInformations();
+
+        sc.close();
+    }
+
 }
--- desafios/src/student/Student.java
+++ desafios/src/student/Student.java
@@ -4,5 +4,11 @@ public class Student {
     String nameOfTheStudent;
     int age = 0;
 
-    
+    void showInformations(){
+        System.out.printf("""
+                \nNome do estudante: %s
+                Idade: %d
+                """, nameOfTheStudent, age);
+    }
+
 }
