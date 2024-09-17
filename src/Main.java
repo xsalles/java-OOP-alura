46ab339101ace78a15a5c9eb94c2e4333ddb1f23
xsalles <xs.salles@gmail.com>
2024-09-16 22:19:36 -0300

    feat: added a values for the atributes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
      public static void main(String[] args) {
         Movie myMovie = new Movie();
+
+        myMovie.name = "Vingadores: Ultimato";
+        myMovie.yearOfRealease = 2019;
+        myMovie.durationInMinutes = 181;
+        myMovie.incluseInPlan = true;
+        myMovie.reviews = 8.1;
+        myMovie.allReviews = 8;
+
+        System.out.println(myMovie.name);
      }
 }
